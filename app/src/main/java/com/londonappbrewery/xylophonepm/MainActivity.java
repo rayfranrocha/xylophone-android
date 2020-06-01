941b829237dc58860d5b4a736b61e3568473706b
rayfran.rocha <rayfran.rocha.lima@gmail.com>
2020-05-31 22:29:19 -0400

    Put code to use SoundPool class

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -4,7 +4,11 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     private final float NORMAL_PLAY_RATE = 1.0f;
 
     // TODO: Add member variables here
+
+    private SoundPool mSoundPool;
+
     private int mCSoundId;
     private int mDSoundId;
     private int mESoundId;
@@ -25,21 +32,42 @@ public class MainActivity extends AppCompatActivity {
     private int mASoundId;
     private int mBSoundId;
 
+    Map<String, Integer> map = new HashMap();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // TODO: Create a new SoundPool
+        mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC, 0);
 
 
         // TODO: Load and get the IDs to identify the sounds
+        mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note1_c, 1);
+        mDSoundId = mSoundPool.load(getApplicationContext(), R.raw.note2_d, 1);
+        mESoundId = mSoundPool.load(getApplicationContext(), R.raw.note3_e, 1);
+        mFSoundId = mSoundPool.load(getApplicationContext(), R.raw.note4_f, 1);
+        mGSoundId = mSoundPool.load(getApplicationContext(), R.raw.note5_g, 1);
+        mASoundId = mSoundPool.load(getApplicationContext(), R.raw.note6_a, 1);
+        mBSoundId = mSoundPool.load(getApplicationContext(), R.raw.note7_b, 1);
 
+        map.put("C", mCSoundId);
+        map.put("D", mDSoundId);
+        map.put("E", mESoundId);
+        map.put("F", mFSoundId);
+        map.put("G", mGSoundId);
+        map.put("A", mASoundId);
+        map.put("B", mBSoundId);
 
     }
 
-    // TODO: Add the play methods triggered by the buttons
+    public void play(View v) {
+        String tag = v.getTag().toString();
+        mSoundPool.play(map.get(tag), 1, 1, 1, 0, 1);
+    }
 
+    // TODO: Add the play methods triggered by the buttons
 
 
 }
